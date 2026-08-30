e8b9ddf1caf2ba83b83c28d99f95a88e35de9a9e
Stepan Mazurov <smazurov@gmail.com>
2026-08-29 23:08:37 -0600

    chore(ui): bind dev server to 127.0.0.1

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -39,7 +39,7 @@ export default defineConfig(() => {
       chunkSizeWarningLimit: 1000
     },
     server: {
-      host: "localhost",
+      host: "127.0.0.1",
       port: parseInt(process.env.VITE_DEV_PORT || "5173", 10),
       proxy: {
         "/api": `http://localhost${process.env.VIDEONODE_SERVER_PORT || ":8090"}`,
